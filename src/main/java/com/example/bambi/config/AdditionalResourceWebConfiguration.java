f88034264329955099d4954f7bc34635696a7ae0
Anish8659 <113288927+Anish8659@users.noreply.github.com>
2023-03-31 13:08:44 +0100

    Update AdditionalResourceWebConfiguration.java

--- src/main/java/com/example/bambi/config/AdditionalResourceWebConfiguration.java
+++ src/main/java/com/example/bambi/config/AdditionalResourceWebConfiguration.java
@@ -7,15 +7,3 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Configuration
-public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        Path brandUploadDir = Paths.get("/static/images");
-        String brandUploadPath = brandUploadDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/images/**").addResourceLocations("file:/" + brandUploadPath + "/");
-
-    }
-}
